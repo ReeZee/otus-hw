e9905e03f3a56d4b497417506985b6f39274fb83
Andrey Balashov <reezee.jooq@gmail.com>
2025-03-24 20:11:24 +0300

    Finish lesson-22

--- lesson-22/src/lib.rs
+++ lesson-22/src/lib.rs
@@ -8,21 +8,58 @@
 // Напишите 1 функцию perimeter_by_area, которая может принимать любую фигуру
 // и возвращает отнощение ее периметра к площади (P/A)
 
-struct Triangle {
-    sides_lens: [f64; 3],
+use std::f64::consts::PI;
+
+pub trait Shape {
+    fn get_area(&self) -> f64;
+    fn get_perimeter(&self) -> f64;
 }
 
-struct Rectangle {
+pub struct Triangle {
+    sides_lens: [f64; 3],
+}
+impl Shape for Triangle {
+    fn get_area(&self) -> f64 {
+        let [a, b, c] = self.sides_lens;
+        let p = (a + b + c) / 2.0;
+        f64::sqrt(p * (p - a) * (p - b) * (p - c))
+    }
+    fn get_perimeter(&self) -> f64 {
+        self.sides_lens[0] + self.sides_lens[1] + self.sides_lens[2]
+    }
+}
+
+pub struct Rectangle {
     width: f64,
     height: f64,
 }
 
-struct Circle {
+impl Shape for Rectangle {
+    fn get_area(&self) -> f64 {
+        self.width * self.height
+    }
+    fn get_perimeter(&self) -> f64 {
+        (self.width + self.height) * 2.0
+    }
+}
+
+pub struct Circle {
     radius: f64,
 }
 
+impl Shape for Circle {
+    fn get_area(&self) -> f64 {
+        (PI * self.radius).powf(2.0)
+    }
+    fn get_perimeter(&self) -> f64 {
+        2.0 * PI * self.radius
+    }
+}
+
 // исправьте сигнатуру и добавьте реализацию
-fn perimeter_by_area(/* */) -> f64 {}
+pub fn perimeter_by_area(s: impl Shape) -> f64 {
+    s.get_perimeter() / s.get_area()
+}
 
 #[cfg(test)]
 mod tests {
@@ -31,14 +68,14 @@ mod tests {
 
     #[test]
     fn test() {
-        relative_eq!(
+        _ = relative_eq!(
             perimeter_by_area(Triangle {
                 sides_lens: [3.0, 4.0, 5.0]
             }),
             2.0
         );
-        relative_eq!(perimeter_by_area(Circle { radius: 2.0 }), 1.0);
-        relative_eq!(
+        _ = relative_eq!(perimeter_by_area(Circle { radius: 2.0 }), 1.0);
+        _ = relative_eq!(
             perimeter_by_area(Rectangle {
                 width: 2.0,
                 height: 3.0,
@@ -47,4 +84,3 @@ mod tests {
         );
     }
 }
-
